2b91701f0cb86f14c37eab443873e5fbf10b0b2b
JulietaNightCode <julietacabrera101@gmail.com>
2023-08-22 22:12:33 -0300

    Datos Primitivos

--- src/repaso/Repaso.java
+++ src/repaso/Repaso.java
@@ -22,13 +22,44 @@ public class Repaso {
         -Se le asignan nombres
         -Asignar tipo de datos
         */
-        
-        int nicoEdad= 38;
-        String nicoNombre = "Nicolás";
+        String nicoNombre = "Nicolás";        
         System.out.println("Me llamo: "+ nicoNombre);
+        
+        //TIPOS DE DATOS PRIMITIVOS
+        /*
+        - 4 tipos de datos que vamos a usar:
+        1) int = almacena números enteros
+        2) float = almacena números decimales de 6 a 7 digitos
+        3) char = almacena un solo caracter/Letra
+        4) boolean = almacena valores verdaderos o falsos
+        Actividad: Hacer Ejemplos con los Datos primitivos
+        */
+        int nicoEdad= 38;        
         System.out.println("Y tengo "+ nicoEdad);
         
-                
+        char riber= 66;
+        char año1=50,año2=48,año3=49,año4=49;
+        System.out.println("River en el año "+año1+año2+año3+año4+" descendió a la "+riber+" nacional!!!");
+        
+        boolean inflacion=false;
+        
+        float precioAzucar= (float)659.99 ;
+        float precioViejoAzucar= (float)149.99;
+        System.out.println("El precio del Azucar actual es de: "+ precioAzucar+"$");
+        
+        if (precioAzucar > precioViejoAzucar) {
+            inflacion=true;
+            System.out.println("El precio del azucar a principio de año era de "+ precioViejoAzucar+"$ pesos");
+            if (inflacion=true){
+                System.out.println("Hay gran inflación en Argentina");
+            }
+        else{
+            System.out.println("No hay nada de inflacion en Argentina :)");
+            }
+        }
+        
+        
+        
                 
     }
     
